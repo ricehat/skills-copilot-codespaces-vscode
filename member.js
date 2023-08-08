8e6cc9c687dce3e45fb6aa30df7d5934e03c3312
ricehat <liu.taoen@gmail.com>
2023-08-08 12:38:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.classList.add("active");
+    var skills = document.getElementById("skills");
+    skills.classList.remove("active");
+    var project = document.getElementById("project");
+    project.classList.remove("active");
+    var contact = document.getElementById("contact");
+    contact.classList.remove("active");
+    var about = document.getElementById("about");
+    about.classList.remove("active");
+    var home = document.getElementById("home");
+    home.classList.remove("active");
+}
